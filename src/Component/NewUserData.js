efcc24f69db2a5c39a6d22c128dcfbe78f283cc7
ShibanshL <shibansh.w@gmail.com>
2022-03-24 12:35:43 +0530

    Here goes nothing

--- src/Component/NewUserData.js
+++ src/Component/NewUserData.js
@@ -2,10 +2,27 @@ import React,{useEffect} from "react";
 import { fetchUserDaataAgain } from "../Redux/NewUser/NewUserAction";
 import { connect } from "react-redux";
 
-function NewUserData(fetchUserDaataAgain) {
-  return (
-    <div>NewUserData</div>
+function NewUserData(userData,fetchUserDaataAgain) {
+    useEffect(()=>{
+        fetchUserDaataAgain()
+    },[])
+     return userData.loading?(
+    <div>
+        userData is Loading
+    </div>
+  ):
+  userData.error ? (
+      <h2>{userData.error}</h2>
+  ):(
+      <div>
+      <h2>User List</h2>
+      <div>
+          {userData && userData.user && userData.user.map(user => <p>{user.name}</p>)}
+          {/* {userData.map(e => <>{e.name}</>)} */}
+      </div>
+      </div>
   )
+
 }
 
 export const mapStateToProps = (state) =>{
